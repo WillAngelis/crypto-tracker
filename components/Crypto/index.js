a4b4d04bb8882eb6ae60393c405219ad9517cc8f
William Angelis <willangelis@gmail.com>
2022-01-13 17:10:45 -0300

    feat: :sparkles: Create Crypto component

--- components/Crypto/index.js
+++ components/Crypto/index.js
@@ -1,10 +1,38 @@
-const Crypto = () => {
+import styles from './Crypto.module.css';
+
+const Crypto = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+  id,
+}) => {
   return (
-    <div>
-      <div>
-        <h1>Cryptos</h1>
-      </div>
-    </div>
+        <div className={styles.coin_container}>
+          <div className={styles.coin_row}>
+            <div className={styles.coin}>
+              <img src={image} alt={name} className={styles.coin_img} />
+              <h1 className={styles.coin_title}>{name}</h1>
+              <p className={styles.coin_symbol}>{symbol}</p>
+            </div>
+            <div className={styles.coin_data}>
+              <p className={styles.coin_price}>R${price.toLocaleString()}</p>
+              <p className={styles.coin_volume}>R${volume.toLocaleString()}</p>
+              {priceChange < 0 ? (
+                <p className={styles.percentRed}>{priceChange.toFixed(2)}%</p>
+              ) : (
+                <p className={styles.percentGreen}>{priceChange.toFixed(2)}%</p>
+              )}
+              <p className={styles.coin_marketcap}>
+                Mkt Cap : R${marketcap.toLocaleString()}
+              </p>
+            </div>
+          </div>
+        </div>
   );
 };
+
 export default Crypto;
