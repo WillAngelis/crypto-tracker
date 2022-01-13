c5ac8ffc9072d62197a2da8a9ed99289a60144c9
William Angelis <willangelis@gmail.com>
2022-01-13 16:44:03 -0300

    feat: :sparkles: Add Crypto component in page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Crypto from '../components/Crypto';
 import SearchBar from '../components/SearchBar';
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div>
       <div>
         <SearchBar type="text" placeholder="Search" />
+        <Crypto />
       </div>
     </div>
   );
