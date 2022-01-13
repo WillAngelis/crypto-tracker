d3d54939669fe9c51bb843ae1885ff210c5ee123
William Angelis <willangelis@gmail.com>
2022-01-13 16:46:54 -0300

    feat: :sparkles: Add API request

--- pages/index.js
+++ pages/index.js
@@ -11,3 +11,17 @@ export default function Home() {
     </div>
   );
 }
+
+const API =
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
+export const getServerSideProps = async () => {
+  const res = await fetch(API);
+  const filteredCoins = await res.json();
+
+  return {
+    props: {
+      filteredCoins,
+    },
+  };
+};
